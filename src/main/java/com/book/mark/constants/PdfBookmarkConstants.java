774e60a05d86dd57c8d16fdd4f25bbfc57a8e22c
Raj Allam <rajagopal.r.allam@hud.gov>
2021-01-10 16:19:20 -0500

    updates

--- src/main/java/com/book/mark/constants/PdfBookmarkConstants.java
+++ src/main/java/com/book/mark/constants/PdfBookmarkConstants.java
@@ -2,9 +2,12 @@ package com.book.mark.constants;
 
 public interface PdfBookmarkConstants {
 	
+	String SUCCESS = "SUCCESS";
+	String FAILED = "FAILED";
 
 	//String url_const = "http://13.72.109.50";
 	String url_const = "https://sandb-ide.appsquared.io/";
+	String url_const_getstatus = "https://sandb-ide.appsquared.io";
 	
 	
 	//String readPdfPathNew = "C:\\Users\\Administrator\\Documents\\Raj\\Bookmarks\\1004_Scanned_0914.pdf";
@@ -24,20 +27,31 @@ public interface PdfBookmarkConstants {
 	String success_message = "Successfully Bookmarked Pdf Document";
 	
 	//String s3_bucket_pdf_files_location = "C:\\Users\\Administrator\\Documents\\GenBookmarks\\MultipleDocs\\";
-	String s3_bucket_pdf_files_location = "C:\\Users\\Administrator\\Documents\\GenBookmarks\\NewTestFile\\";
+	//String s3_bucket_pdf_files_location = "C:\\Users\\Administrator\\Documents\\GenBookmarks\\NewTestFile\\";
+	//ABOVE LINE IS MOVED TO APPLICATION_PROPERTIES FILE--DEC20_2020
 	//String MergedDocumentPath = "C:\\Users\\Administrator\\Documents\\GenBookmarks\\MergeMultipleDocs\\MergedPdf.pdf";//How to generate the Merged File Name(As of now Hardcoding)
-	String MergedDocumentPath = "C:\\Users\\Administrator\\Documents\\GenBookmarks\\MergeMultipleDocs\\";
-	
+	//String MergedDocumentPath = "C:\\Users\\Administrator\\Documents\\GenBookmarks\\MergeMultipleDocs\\";
+	//ABOVE LINE IS MOVED TO APPLICATION_PROPERTIES FILE--DEC20_2020
+	//String ExtractFormNamesLocation = "C:\\Users\\Administrator\\Documents\\GenBookmarks\\ExtractFormNames\\";
+	//ABOVE LINE IS MOVED TO APPLICATION_PROPERTIES FILE--DEC20_2020
 	String generateCSV = "GenCSV";
 	String bookMarkPdf = "BookMarkPdf";
 	String genPdfgenCsv = "Both";
-	String genCsvFilePath = "C:\\Users\\Administrator\\Documents\\GenBookmarks\\CSVFiles\\CsvData.csv";
-	
+	String presenceFormNames = "presenceFormNames";
+	String extractionFormNames = "extractionFormNames";
+	//String genCsvFilePath = "C:\\Users\\Administrator\\Documents\\GenBookmarks\\CSVFiles\\CsvData.csv";
+	//ABOVE LINE IS MOVED TO APPLICATION_PROPERTIES FILE--DEC20_2020
 	String NEWLINE = "\n";
 	String COMPLETE = "COMPLETE";
 	
 	long PDF_TIMEOUT=80;
 	
 	String Authorization_Token = "Token 984910a508f30eb63dfe404aa8e2d4acf505ae14";
+	
+	String AWS_BUCKET_NAME = "ide-sandb-temp-microservice-bucket";
+	
+	String ACCESS_KEY = "AKIAXM6VM7LNUSMVFKRH";
+	String SECRET_KEY = "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";
+	String SESSION_TOKEN = "";
 	
 }
